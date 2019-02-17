f03157644ba1f2c47535ac70fec04247ae9f8273
Sz-Ting, Chen <kimikamis@gmail.com>
2019-02-17 20:53:31 +0800

    create project / make basic function

---
+++ app.js
@@ -0,0 +1,19 @@
+// add row
+function addHtmlTableRow() {
+  let table = document.getElementById("table"),
+  newRow = table.insertRow(table.length),
+  cell1 = newRow.insert(0),
+  cell2 = newRow.insert(0),
+  cell3 = newRow.insert(0),
+  cell4 = newRow.insert(0),
+  cell5 = newRow.insert(0),
+  name = document.getElementById("name"),
+  phone = document.getElementById("phone"),
+  email = document.getElementById("email");
+
+  console.log(name)
+}
+
+// edit row
+
+// delete row
